fa7777d73c771eeb9c428f2b0b5f0e69fff922fc
Софья Клочихина <93037748+sklochikhina@users.noreply.github.com>
2022-12-12 12:22:21 +0700

    init

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <exception>
+#include <iostream>
+
+#include "game_runner.h"
+
+int main(int argc, char* argv) {
+
+	if (argc < 4) {
+		std::cerr << "Invalid number of arguments: names of at least 3 strategies" << std::endl;
+		CLI ui;
+		ui.print_help();
+		return -1;
+	}
+
+	try {
+		CLI ui;				// user input
+		ui.print_help();
+
+		Arguments args = ui.parsing_args(argc, argv);	// почему читает русские символы, которых нет??
+
+		Runner* runner;
+
+		if (Mode::DETAILED == args.mode)
+			runner = new Detailed_runner(read_matrix(args.matrix_file), args.strategies);
+		else
+			runner = new Fast_runner(read_matrix(args.matrix_file), args.strategies, args.steps);
+
+		runner->run(ui);
+	}
+
+	catch (std::exception) {
+		std::cerr << "Caught some exception" << std::endl;
+		return -1;
+	}
+
+	return 0;
+}
