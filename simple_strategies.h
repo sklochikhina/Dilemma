ba33e2049d3ae852f85dab31dcbdbc8971b34c3d
Софья Клочихина <93037748+sklochikhina@users.noreply.github.com>
2022-12-12 12:39:25 +0700

    init

---
+++ simple_strategies.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <random>
+
+#include "strategy.h"
+
+class Betray : public Strategy {	// всегда предаёт
+public:
+	void make_choice() override;
+	Choice get_choice() override;
+	void handle_result(const Result& res) override;
+private:
+	Choice _choice = Choice::BETRAY;
+};
+
+class Cooperate : public Strategy {	// всегда сотрудничает
+public:
+	void make_choice() override;
+	Choice get_choice() override;
+	void handle_result(const Result& res) override;
+private:
+	Choice _choice = Choice::COOPERATE;
+};
+
+class Random : public Strategy {	// выбирает случайно
+public:
+	void make_choice() override;
+	Choice get_choice() override;
+	void handle_result(const Result& res) override;
+private:
+	Choice _choice = Choice::COOPERATE;
+};
+
+class Change : public Strategy {	// постоянно меняет решение
+public:
+	void make_choice() override;
+	Choice get_choice() override;
+	void handle_result(const Result& res) override;
+private:
+	Choice _choice = Choice::COOPERATE;
+};
