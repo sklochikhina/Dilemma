b53c551341967716664d753896b7029ca1d58e13
Софья Клочихина <93037748+sklochikhina@users.noreply.github.com>
2022-12-12 12:38:55 +0700

    init

---
+++ game_runner.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <algorithm>
+
+#include "matrix.h"
+#include "game_runner.h"
+
+std::vector<Strategy*> make_strategies_from_names(const std::vector<std::string>& names) {
+	// добавить проверку на допустимый размер вектора? //
+	std::vector<Strategy*> strategies(COLS);
+
+	for (auto& name : names)
+		strategies.push_back(create_strategy(name));
+
+	return strategies;
+}
+
+Detailed_runner::Detailed_runner(const Matrix& matrix, std::vector<std::string> names) :
+	game(matrix, make_strategies_from_names(names)),
+	_names(std::move(names)) {}
+
+Fast_runner::Fast_runner(const Matrix& matrix, std::vector<std::string> names, std::size_t steps) :
+	game(matrix, make_strategies_from_names(names)),
+	_names(std::move(names)),
+	_steps(steps) {}
+
+void Detailed_runner::run(CLI& interface) {
+	while (interface.read_comand()) {
+		game.step();
+		interface.print_intermediate(_names, game.get_result());
+	}
+	interface.print_after_game(_names, game.get_result());
+}
+
+void Fast_runner::run(CLI& interface) {
+	for (std::size_t i = 0; i < _steps; i++)
+		game.step();
+	interface.print_after_game(_names, game.get_result());
+}
