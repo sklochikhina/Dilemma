2e6d476be2a65a82ba0a50b916e741bf35b363eb
Софья Клочихина <93037748+sklochikhina@users.noreply.github.com>
2022-12-12 12:24:44 +0700

    init

---
+++ strategy.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+
+const std::size_t COLS = 3;	// columns
+
+enum class Choice {
+	COOPERATE = 0,
+	BETRAY = 1,
+};
+
+struct Result {
+	Result(int cols = COLS);
+
+	std::vector<Choice> _choices;
+	std::vector<int> _payoffs;
+	std::vector<int> _scores;
+};
+
+class Strategy {
+public:
+	virtual void make_choice() = 0;
+	virtual Choice get_choice() = 0;
+	virtual void handle_result(const Result& res) = 0;
+	virtual ~Strategy() {};
+};
