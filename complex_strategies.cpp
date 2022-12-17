9c04e06ec0ea86d746c890018c64d8eab2ee9527
Софья Клочихина <93037748+sklochikhina@users.noreply.github.com>
2022-12-17 19:02:07 +0700

    определила метод handle_result
    надеюсь, это работает

--- complex_strategies.cpp
+++ complex_strategies.cpp
@@ -3,7 +3,14 @@
 
 Choice Majority::get_choice() { return _choice; }
 
-void Majority::handle_result(const Result& res) {}
+void Majority::handle_result(const Result& res) {
+	for (auto& choice : res._choices) {
+		if (Choice::COOPERATE == choice)
+			num_coop++;
+		else
+			num_betr++;
+	}
+}
 
 void Majority::make_choice() {
 	(num_coop > num_betr) ? _choice = Choice::COOPERATE : _choice = Choice::BETRAY;
