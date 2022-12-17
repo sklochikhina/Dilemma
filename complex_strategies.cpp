969028e34d7e8e1a11874077c4d8b8222d8463c8
Софья Клочихина <93037748+sklochikhina@users.noreply.github.com>
2022-12-17 18:47:05 +0700

    определила метод make_choice

--- complex_strategies.cpp
+++ complex_strategies.cpp
@@ -5,4 +5,6 @@ Choice Majority::get_choice() { return _choice; }
 
 void Majority::handle_result(const Result& res) {}
 
-void Majority::make_choice() {}
+void Majority::make_choice() {
+	(num_coop > num_betr) ? _choice = Choice::COOPERATE : _choice = Choice::BETRAY;
+}
