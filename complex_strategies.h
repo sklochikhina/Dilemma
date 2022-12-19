61016efb33fa3e3862e7566358c8200b49f93a4b
Софья Клочихина <93037748+sklochikhina@users.noreply.github.com>
2022-12-19 14:50:13 +0700

    +сложная стратегия

--- complex_strategies.h
+++ complex_strategies.h
@@ -12,3 +12,12 @@ private:
 	std::size_t num_coop = 0;	// сколько стратегий сотрудничали в предыдущем раунде
 	std::size_t num_betr = 0;	// сколько стратегий предали в предыдущем раунде
 };
+
+class Mimic : public Strategy {		// подражает той стратегии, которая на предыдущем раунде набрала большее количество очков
+public:
+	void make_choice() override;
+	Choice get_choice() override;
+	void handle_result(const Result& res) override;
+private:
+	Choice _choice = Choice::COOPERATE;
+};
