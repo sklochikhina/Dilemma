6526406089d7a6bfa66dfcea5e11365e54733524
Софья Клочихина <93037748+sklochikhina@users.noreply.github.com>
2022-12-22 22:13:48 +0700

    поправочки

--- simple_strategies.cpp
+++ simple_strategies.cpp
@@ -4,39 +4,32 @@
 
 #include "simple_strategies.h"
 
-Choice Betray::get_choice() { return _choice; }
+
+Choice Betray::make_choice() { return Choice::BETRAY; }
 
 void Betray::handle_result(const Result& res) {}
 
-void Betray::make_choice() { _choice = Choice::BETRAY; }
 
-
-Choice Cooperate::get_choice() { return _choice; }
+Choice Cooperate::make_choice() { return Choice::COOPERATE; }
 
 void Cooperate::handle_result(const Result& res) {}
 
-void Cooperate::make_choice() { _choice = Choice::COOPERATE; }
 
-
-Choice Random::get_choice() { return _choice; }
+Choice Random::make_choice() {
+	std::srand(time(nullptr));
+	bool choice = std::rand() % 2;
+	return (0 == choice) ? Choice::COOPERATE : Choice::BETRAY;
+}
 
 void Random::handle_result(const Result& res) {}
 
-void Random::make_choice() {
-	std::srand(time(nullptr));
-	bool choice = std::rand() % 2;
-	(0 == choice) ? _choice = Choice::COOPERATE : _choice = Choice::BETRAY;
+
+Choice Change::make_choice() {
+	if (_choice == Choice::COOPERATE)
+		_choice = Choice::BETRAY;
+	else
+		_choice = Choice::COOPERATE;
+	return _choice;
 }
-
-
-Choice Change::get_choice() { return _choice; }
 
 void Change::handle_result(const Result& res) {}
-
-void Change::make_choice() {
-	if (_choice == Choice::COOPERATE) {
-		_choice = Choice::BETRAY;
-		return;
-	}
-	_choice = Choice::COOPERATE;
-}
