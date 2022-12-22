368ccbf1ec5c2389ccd99ff287fada3838d5e4e8
Софья Клочихина <93037748+sklochikhina@users.noreply.github.com>
2022-12-22 22:14:42 +0700

    убрала метод get_choice()

--- complex_strategies.h
+++ complex_strategies.h
@@ -4,19 +4,16 @@
 
 class Majority : public Strategy {	//  выбирает ту же стратегию, что и большинство
 public:
-	void make_choice() override;
-	Choice get_choice() override;
+	Choice make_choice() override;
 	void handle_result(const Result& res) override;
 private:
-	Choice _choice = Choice::COOPERATE;
 	std::size_t num_coop = 0;	// сколько стратегий сотрудничали в предыдущем раунде
 	std::size_t num_betr = 0;	// сколько стратегий предали в предыдущем раунде
 };
 
 class Mimic : public Strategy {		// подражает той стратегии, которая на предыдущем раунде набрала большее количество очков
 public:
-	void make_choice() override;
-	Choice get_choice() override;
+	Choice make_choice() override;
 	void handle_result(const Result& res) override;
 private:
 	Choice _choice = Choice::COOPERATE;
