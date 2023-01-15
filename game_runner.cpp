4eda6180fb0f669936b98b4d0add548a392d3763
Софья Клочихина <93037748+sklochikhina@users.noreply.github.com>
2023-01-15 22:05:06 +0700

    ...

--- game_runner.cpp
+++ game_runner.cpp
@@ -4,22 +4,22 @@
 #include "matrix.h"
 #include "game_runner.h"
 
-std::vector<Strategy*> make_strategies_from_names(const std::vector<std::string>& names) {
+std::vector<Strategy*>* make_strategies_from_names(const std::vector<std::string>& names) {
 	assert(COLS == names.size());
-	std::vector<Strategy*> strategies;
-	strategies.reserve(COLS);
+	std::vector<Strategy*>* strategies = new std::vector<Strategy*>;
+	strategies->reserve(COLS);
 
 	for (auto& name : names)
-		strategies.push_back(create_strategy(name));
+		strategies->push_back(create_strategy(name));
 
 	return strategies;
 }
 
-DetailedRunner::DetailedRunner(const Matrix& matrix, std::vector<std::string> names) :
+DetailedRunner::DetailedRunner(const Matrix& matrix, std::vector<std::string>& names) :
 	game(matrix, make_strategies_from_names(names)),
 	_names(std::move(names)) {}
 
-FastRunner::FastRunner(const Matrix& matrix, std::vector<std::string> names, std::size_t steps) :
+FastRunner::FastRunner(const Matrix& matrix, std::vector<std::string>& names, std::size_t& steps) :
 	game(matrix, make_strategies_from_names(names)),
 	_names(std::move(names)),
 	_steps(steps) {}
