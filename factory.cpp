eb9cb9acafa4801bc6b1ac8ea506d447e8990bf0
Софья Клочихина <93037748+sklochikhina@users.noreply.github.com>
2023-01-15 21:40:44 +0700

    переместила всё в cpp-файл

--- factory.cpp
+++ factory.cpp
@@ -25,3 +25,25 @@ Strategy* Factory::create(const std::string& name) {
 		return new Mimic;
 	}
 }
+
+std::vector<std::string> _creators = { "cooperate",
+										"betray",
+										"random",
+										"change",
+										"majority",
+										"mimic", };
+
+bool search_strategy_by_id(const std::string& id) {
+	auto it = std::find(_creators.begin(), _creators.end(), id);
+
+	if (it == _creators.end())
+		throw std::invalid_argument("Invalid id for the factory");
+
+	return true;
+}
+
+Strategy* create_strategy(const std::string& id) {
+	search_strategy_by_id(id);
+	Factory* strategy{};
+	return strategy->create(id);
+}
